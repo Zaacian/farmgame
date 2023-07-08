90f8bfb10fcc05cc0339a67d13184c6e27b7d859
Zaacian <posawat1597@gmail.com>
2023-07-08 14:42:29 +0700

    CreateBuilding

---
+++ Assets/scripts/Managers/StructureManager.cs
@@ -0,0 +1,91 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StructureManager : MonoBehaviour
+{
+    [SerializeField] private bool isConstructing;
+    [SerializeField] private bool isDemolishing;
+
+    [SerializeField] private GameObject curBuildingPrefab;
+    [SerializeField] private GameObject buildingParent;
+
+    [SerializeField] private Vector3 curCursorPos;
+
+    public GameObject buildingCursor;
+    public GameObject gridPlane;
+
+    private GameObject ghostBuilding;
+
+    [SerializeField] private GameObject _curStructure; //Currently selected structure
+    public GameObject CurStructure { get { return _curStructure; } set { _curStructure = value; } }
+
+    [SerializeField] private GameObject[] structurePrefab;
+
+    private Camera cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        curCursorPos = Formula.instance.GetCurTilePosition();
+
+        if (isConstructing) //Mode Construct
+        {
+            buildingCursor.transform.position = curCursorPos;
+            gridPlane.SetActive(true);
+        }
+        else //Mode Play
+        {
+            gridPlane.SetActive(false);
+        }
+    }
+
+    public void BeginNewBuildingPlacement(GameObject prefab)
+    {
+        isDemolishing = false;
+        isConstructing = true;
+
+        curBuildingPrefab = prefab;
+
+        //Instantiage Ghost Building
+        ghostBuilding = Instantiate(curBuildingPrefab, curCursorPos, Quaternion.identity);
+        ghostBuilding.GetComponent<FindBuildingSite>().Plane.SetActive(true);
+
+        buildingCursor = ghostBuilding;
+        buildingCursor.SetActive(true);
+    }
+    private void PlaceBuilding()
+    {
+        if (buildingCursor.GetComponent<FindBuildingSite>().CanBuild == false)
+            return;
+
+        GameObject structureObj = Instantiate(curBuildingPrefab,
+                                               curCursorPos,
+                                               Quaternion.identity,
+                                               buildingParent.transform);
+
+        Structure s = structureObj.GetComponent<Structure>();
+
+        //Add building in Office
+        //Deduct Money
+    }
+
+    private void CheckLeftClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (isConstructing)
+                PlaceBuilding(); //Real Construction
+        }
+    }
+
+}
+
+
