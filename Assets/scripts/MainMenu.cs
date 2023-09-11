aa171d3fb101cee64295067d2000eb7979e32dcd
Zaacian <posawat1597@gmail.com>
2023-09-11 09:15:37 +0700

    StartNewGame

---
+++ Assets/scripts/MainMenu.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] string mapScene;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void StartNewGame()
+    {
+        SceneManager.LoadScene(mapScene);
+    }
+    public void ExitScence()
+    {
+
+    }
+}
